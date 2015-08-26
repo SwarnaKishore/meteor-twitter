faaff0059ae6eb6566f9295ffc86de207fd8a363
Swarna Kishore <swarnakishoree@gmail.com>
2015-08-25 20:30:45 -0500

    added templates

--- meteor-twitter.js
+++ meteor-twitter.js
@@ -1,19 +1,50 @@
+Tweets = new Meteor.Collection("tweets");  
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  Template.tweetBox.onRendered(function () {  
+  Session.set('numChars', 0);
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+
+  Template.tweetBox.helpers({  
+  charCount: function() {
+    return 140 - Session.get('numChars');
+  },
+
+  charClass: function() {
+    if (Session.get('numChars') > 140) {
+      return 'errCharCount';    //css class name
+    } else {
+      return 'charCount';       //css class name
     }
-  });
+  },
+
+  disableButton: function() {
+    if (Session.get('numChars') <= 0 ||
+        Session.get('numChars') > 140) {
+      return 'disabled';
+    }
+  }
+});
+
+
+  Template.tweetBox.events({  
+  'input #tweetText': function(){
+    Session.set('numChars', $('#tweetText').val().length);
+  },
+
+  'click button': function() {  
+  var tweet = $('#tweetText').val();
+  $('#tweetText').val("");
+  Session.set('numChars', 0);
+  Tweets.insert({message: tweet});
+}
+
+});
+
+
 }
 
 if (Meteor.isServer) {
